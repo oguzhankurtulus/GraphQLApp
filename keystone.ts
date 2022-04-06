96d156e6aa506c199450a58f86ed16ba0a3fb30d
Oguzhan Kurtulus <oguzhan.kurtulus@yandex.com>
2022-04-07 02:05:33 +0300

    Add ProductImage schema. Develop Product service.

--- keystone.ts
+++ keystone.ts
@@ -5,7 +5,10 @@ import {
 import { createAuth } from '@keystone-next/auth';
 import { config, createSchema } from '@keystone-next/keystone/schema';
 import { User } from './schemas/User';
+import { Product } from './schemas/Product';
+import { ProductImage } from './schemas/ProductImage';
 import 'dotenv/config';
+import { insertSeedData } from './seed-data';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
@@ -36,10 +39,17 @@ export default withAuth(
     db: {
       adapter: 'mongoose',
       url: databaseURL,
+      async onConnect(keystone) {
+        if (process.argv.includes('--seed-data')) {
+          await insertSeedData(keystone);
+        }
+      },
       // TODO: Add data seeding here
     },
     lists: createSchema({
       User,
+      Product,
+      ProductImage,
       // Schema items go in here
     }),
     ui: {
